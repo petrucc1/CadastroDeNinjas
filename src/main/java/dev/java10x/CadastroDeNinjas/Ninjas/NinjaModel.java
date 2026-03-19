aae1b33bb4087d79c3f073f96c9c5a57181a9da4
Sarah Petrucci <petrucci_@outlook.com.br>
2026-03-19 17:27:32 -0300

    fix: Adiciona o @Column na classe NinjaModel e corrige o tipo de classe do NinjaRepository

--- src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaModel.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaModel.java
@@ -26,6 +26,9 @@ public class NinjaModel {
     @Column(unique = true)
     private String email;
 
+    @Column(name = "img_url")
+    private String imgUrl;
+
     private int idade;
 
     @ManyToOne // Um ninja só pode ter uma missão
