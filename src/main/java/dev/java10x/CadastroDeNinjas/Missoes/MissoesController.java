18180abe8db61ed9302e4781364a06d994d4dc64
Sarah Petrucci <petrucci_@outlook.com.br>
2026-03-23 20:32:01 -0300

    feat: Atualiza o Controller de missões com mock de dados.

--- src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissoesController.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissoesController.java
@@ -1,4 +1,32 @@
 package dev.java10x.CadastroDeNinjas.Missoes;
 
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("missoes")
 public class MissoesController {
+    // CRUD
+    // Criar uma missão (CREATE)
+    @PostMapping("/criar")
+    public String criarMissao() {
+        return "Missão criada!";
+    }
+
+    // Listar todas as missões (READ)
+    @GetMapping("/todas")
+    public String mostrarMissoes() {
+        return "Missões listadas!";
+    }
+
+    // Alterar dados das missões (UPDATE)
+    @PutMapping("/alterar")
+    public String alterarMissao() {
+        return "Missão alterada!";
+    }
+
+    // Deletar uma missão (DELETE)
+    @DeleteMapping("/deletar{id}")
+    public String deletarMissao() {
+        return "Missão deletada!";
+    }
 }
