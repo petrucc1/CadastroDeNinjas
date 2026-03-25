5a9ab46af5bdd7e2f11b1396d069ccfb5daf36ee
Sarah Petrucci <petrucci_@outlook.com.br>
2026-03-25 15:23:53 -0300

    feat: Inclui DTO como abstração para a Entidade nas camadas de Service e Controller.

--- src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaController.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaController.java
@@ -16,7 +16,7 @@ public class NinjaController {
     // CRUD
     // Cria um ninja (CREATE)
     @PostMapping("/criar")
-    public NinjaModel criarNinja(@RequestBody NinjaModel ninja) {
+    public NinjaDTO criarNinja(@RequestBody NinjaDTO ninja) {
         return ninjaService.criarNinja(ninja);
     }
 
